ba172e2033e9eb86825863a28a819efe9eb88133
Hunter Cote <hcote12@gmail.com>
2019-06-30 17:28:24 -0400

    added favicon, css styling

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
-  console.log('sanity check');
 
 var about = document.getElementById("about-bar");
 var port = document.getElementById("port-bar");
 var exp = document.getElementById("exp-bar");
 var edu = document.getElementById("edu-bar");
-console.log(window.pageYOffset);
+var arrow = document.getElementsByClassName("fa-angle-double-down")[0]
 
 about.className = 'scroll';
 
 window.addEventListener('scroll', () => {
-  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 10) {
+    arrow.classList.add('hide');
+  } else {
+    arrow.classList.remove('hide');
+  }
   if (window.pageYOffset >= 0 && window.pageYOffset <= 850) {
     about.classList.add("scroll", "side-panel");
   } else {
     about.classList.remove("scroll");
     about.classList.add("side-panel");
   }
-  if (window.pageYOffset > 850 && window.pageYOffset <= 4620) {
+  if (window.pageYOffset > 850 && window.pageYOffset <= 3800) {
     port.className = 'scroll';
   } else {
     port.classList.remove("scroll");
     port.classList.add("side-panel");
   }
-  if (window.pageYOffset > 4620 && window.pageYOffset <= 5460) {
+  if (window.pageYOffset > 3800 && window.pageYOffset <= 4810) {
     exp.className = 'scroll';
   } else {
     exp.classList.remove("scroll");
     exp.classList.add("side-panel");
   }
-  if (window.pageYOffset > 5460) {
+  if (window.pageYOffset > 4810) {
     edu.className = 'scroll';
   } else {
     edu.classList.remove("scroll");
